f791da0fbee222aadce6d917fc1cb710c3e29c40
Nathan Woloshyn <nathanwoloshyn@gmail.com>
2024-06-17 21:25:52 -0700

    basic negamax working!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use cozy_chess::*;
-use rand::Rng;
+// use rand::Rng;
 fn main() {
     // Simple implementation of the UCI protocol
     //read from stdin and write to stdout. If a move is requested, generate a random move
@@ -57,16 +57,7 @@ fn main() {
         //            _ => {}
         //        }
         //     }
-            
-            // // Generate a random move
-            // let mut move_list = Vec::new();
-            // board.generate_moves(|moves| {
-            //     // Unpack dense move set into move list
-            //     move_list.extend(moves);
-            //     false
-            // });
-            // let random_index = rng.gen_range(0..move_list.len());
-            let best_move = get_best_move(&board);
+            let best_move = get_best_move(&board, 3, board.side_to_move());
             println!("bestmove {}", best_move);
 
         }
@@ -76,13 +67,87 @@ fn main() {
     }
 }
 
-fn get_best_move(board: &Board) -> String {
-    let mut move_list = Vec::new();
-    board.generate_moves(|moves| {
-        // Unpack dense move set into move list
-        move_list.extend(moves);
+// fn get_random_move(board: &Board) -> String {
+//     let mut move_list = Vec::new();
+//     board.generate_moves(|moves| {
+//         // Unpack dense move set into move list
+//         move_list.extend(moves);
+//         false
+//     });
+//     let random_index = rand::thread_rng().gen_range(0..move_list.len());
+//     return move_list[random_index].to_string()
+// }
+
+fn count_material(board: &Board, color: Color ) -> i32 {
+    let mut material: i32 = 0;
+    //create static array of each piece type
+    let pieces: [Piece; 5] = [Piece::Pawn, Piece::Knight, Piece::Bishop, Piece::Rook, Piece::Queen];
+    let values: [i32; 5] = [1, 3, 3, 5, 9];
+    for (i, piece) in pieces.iter().enumerate() {
+        let count: i32 = board.colored_pieces(color, *piece).len() as i32;
+        material += count * values[i];
+    }
+    return material;
+}
+
+fn evaluate(board: &Board, stm: Color) -> i32 {
+    let mut score: i32;
+    let white_material = count_material(board, Color::White);
+    let black_material = count_material(board, Color::Black);
+    score = white_material - black_material;
+    if stm == Color::Black {
+        score = -score;
+    }
+    return score;
+}
+
+fn negamax(board: &Board, depth: i32, alpha: i32, beta: i32, color: Color) -> i32 {
+    let status: GameStatus = board.status();
+    //match on status for draw or checkmate
+    if status != GameStatus::Ongoing {
+        match status {
+            GameStatus::Won => {
+                return -999;
+            }
+            GameStatus::Drawn => return 0,
+            _ => {}
+        }
+    }
+    if depth == 0 {
+        return evaluate(board, color);
+    }
+    let mut best_score = -999;
+    let mut new_alpha = alpha;
+    board.generate_moves(|moves: PieceMoves| {
+        for m in moves {
+            let mut new_board: Board = board.clone();
+            new_board.play(m);
+            let score: i32 = -negamax(&new_board, depth - 1, -beta, -new_alpha, !color);
+            best_score = best_score.max(score);
+            new_alpha = new_alpha.max(score);
+            if new_alpha >= beta {
+                break;
+            }
+        }
         false
     });
-    let random_index = rand::thread_rng().gen_range(0..move_list.len());
-    move_list[random_index].to_string()
+    return best_score;
+}
+
+fn get_best_move(board: &Board, depth: i32, color: Color) -> String {
+    let mut best_move = String::new();
+    let mut best_score = -1000;
+    board.generate_moves(|moves| {
+        for m in moves {
+            let mut new_board = board.clone();
+            new_board.play(m);
+            let score = -negamax(&new_board, depth - 1, -1000, 1000, !color);
+            if score > best_score {
+                best_score = score;
+                best_move = m.to_string();
+            }
+        }
+        false
+    });
+    return best_move;
 }
